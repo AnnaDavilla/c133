659caebab12e8d4bd84b4501da72568d04263c8a
Anna Dávilla <110867038+AnnaDavilla@users.noreply.github.com>
2023-12-04 22:40:34 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+# importando os módulos da biblioteca flask
+from flask import Flask , render_template
+
+# criando a instância da classe Flask, fornecendo a palavra-chave __name__ como argumento
+app = Flask(__name__)
+
+# escreva as rotas usando funções de decorador
+# rota padrão ou 'URL'
+@app.route("/")
+def home():
+
+    nome = "Alex" # escreva seu nome
+    idade = "12" # escreva sua idade
+
+    return render_template('index.html' , nome = nome , idade = idade)
+
+# defina a rota para a página do pai
+@app.route("/pai")
+def pai():
+
+    nome = "pai" # escreva seu nome
+    idade = "50" # escreva sua idade
+
+    return render_template('pai.html' , nome = nome , idade = idade)
+
+# defina a rota para a página da mãe
+@app.route("/mãe")
+def mãe():
+
+    nome = "mãe" # escreva seu nome
+    idade = "30" # escreva sua idade
+
+    return render_template('mae.html' , nome = nome , idade = idade)
+
+# defina a rota para a página do amigo
+@app.route("/amigo")
+def amigo():
+
+    nome = "amigo" # escreva seu nome
+    idade = "10" # escreva sua idade
+
+    return render_template('amigo.html' , nome = nome , idade = idade)
+
+# adicione outras rotas, se você quiser
+
+# execute o arquivo
+if __name__  ==  '__main__':
+    app.run(debug=True)
